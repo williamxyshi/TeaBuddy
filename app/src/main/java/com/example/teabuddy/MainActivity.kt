486e025ad5458c7cb580a012b9da09abe0a8b5fc
Will <williamxyshi@gmail.com>
2019-12-01 19:11:13 -0500

    added button!

--- app/src/main/java/com/example/teabuddy/MainActivity.kt
+++ app/src/main/java/com/example/teabuddy/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.teabuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+
+        button.
     }
+
+
+
+
+
+
 }
