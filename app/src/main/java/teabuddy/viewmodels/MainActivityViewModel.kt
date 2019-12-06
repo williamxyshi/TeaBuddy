5224ddb93f399785f39db477f0737eaf16c641c6
williamxyshi <williamxyshi@gmail.com>
2019-12-05 21:16:45 -0500

    added Realm dependency, implemented application() class and realmmigrations file

--- app/src/main/java/teabuddy/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/teabuddy/viewmodels/MainActivityViewModel.kt
@@ -4,10 +4,12 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.realm.Realm
 
 class MainActivityViewModel(application: Application): AndroidViewModel(application) {
 
     var currentActionPage: MutableLiveData<Int> = MutableLiveData()
+    val realm = Realm.getDefaultInstance()
 
     companion object {
         const val DASHBOARD_PAGE = 0
