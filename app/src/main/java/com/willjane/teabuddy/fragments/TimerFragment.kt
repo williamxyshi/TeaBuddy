65ae5bde2fdc24db3548b88ea6c474d47fedbd40
jane-le <janele913@gmail.com>
2019-12-09 21:57:55 -0500

    set timer functionality

--- app/src/main/java/com/willjane/teabuddy/fragments/TimerFragment.kt
+++ app/src/main/java/com/willjane/teabuddy/fragments/TimerFragment.kt
@@ -10,12 +10,9 @@ import androidx.lifecycle.ViewModelProviders
 import com.willjane.teabuddy.R
 import com.willjane.teabuddy.viewmodels.MainActivityViewModel
 import android.os.CountDownTimer
-import android.text.InputFilter
-import android.widget.Button
-import android.widget.TextView
-import io.opencensus.stats.Aggregation
-import kotlinx.android.synthetic.main.fragment_teatimer.view.*
-import java.util.*
+import android.view.Gravity
+import android.widget.*
+import kotlinx.android.synthetic.main.fragment_teatimer.*
 
 
 class TeaTimerFragment: Fragment() {
@@ -24,9 +21,13 @@ class TeaTimerFragment: Fragment() {
     private lateinit var timerSec: TextView
     private lateinit var startTime: Button
     private lateinit var stopTime: Button
-    private var timerLength: Long = 180000
+    private lateinit var openSetTime: Button
+    private var timerLength: Long = 0
     private lateinit var countDownTimer: CountDownTimer
     private var isInitStart = false
+    private lateinit var popupView: ViewGroup
+    private lateinit var setMinutes: NumberPicker
+    private lateinit var setSeconds: NumberPicker
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,12 +36,18 @@ class TeaTimerFragment: Fragment() {
     ): View? {
         Log.d(TAG, "loading tea timer fragment")
         val rootView = inflater.inflate(R.layout.fragment_teatimer, container, false) as ViewGroup
+        popupView = inflater.inflate(R.layout.set_teatimer, null) as ViewGroup
+
         timerMins = rootView.findViewById(R.id.timerMin)
         timerSec = rootView.findViewById(R.id.timerSec)
         startTime = rootView.findViewById(R.id.startTimer)
         stopTime = rootView.findViewById(R.id.stopTimer)
-        setMinutes(timerLength)
-        setSeconds(timerLength)
+        openSetTime = rootView.findViewById(R.id.openSetTimer)
+        setMinutes = popupView.findViewById(R.id.setMinutes)
+        setSeconds = popupView.findViewById(R.id.setSeconds)
+
+        setMinText(timerLength)
+        setSecText(timerLength)
         initializeTimer(timerLength)
         initialize()
 
@@ -58,17 +65,23 @@ class TeaTimerFragment: Fragment() {
         stopTime.setOnClickListener {
             stopTimer()
         }
+
+        openSetTimer.setOnClickListener {
+            initPopup()
+        }
+
     }
 
     private fun initializeTimer(timeLeft : Long) {
         countDownTimer = object : CountDownTimer(timeLeft, 1000) {
 
             override fun onTick(millisUntilFinished: Long) {
-                setMinutes(millisUntilFinished)
-                setSeconds(millisUntilFinished)
+                setMinText(millisUntilFinished)
+                setSecText(millisUntilFinished)
                 timerLength = millisUntilFinished
             }
             override fun onFinish() {
+                //TODO do something when timer finishes
                 timerSec.text = "done"
             }
 
@@ -86,20 +99,38 @@ class TeaTimerFragment: Fragment() {
 
     }
 
-    private fun setMinutes(millSec: Long) {
-        timerMins.text = millToMin(millSec)
-    }
-    private fun setSeconds(millSec: Long) {
-        timerSec.text = millToSec(millSec)
+    private fun setMinText(millSec: Long) {
+        timerMins.text = ((millSec / 60000) % 10).toString()
     }
 
-    private fun millToMin(millSec : Long) : String {
-        return (((millSec / 60000) % 10).toString())
+    private fun setSecText(millSec: Long) {
+        timerSec.text = ((millSec / 1000) % 60 ).toString()
     }
 
-    private fun millToSec(millSec : Long) : String {
-        return (((millSec / 1000) % 60 ).toString())
+    private fun initPopup() {
+        setMinutes.value = (timerLength / 60000 % 10).toInt()
+        setSeconds.value = (timerLength / 1000 % 60).toInt()
+        setMinutes.minValue = 0
+        setMinutes.maxValue = 5
+        setSeconds.minValue = 0
+        setSeconds.maxValue = 59
+
+        val window = PopupWindow(context)
+        window.contentView = popupView
+        val closeSetTime =  popupView.findViewById<Button>(R.id.closeSetTimer)
+        closeSetTime.setOnClickListener{
+            window.dismiss()
+        }
+        val setTimer = popupView.findViewById<Button>(R.id.setTimer)
+        setTimer.setOnClickListener {
+            timerLength = (setMinutes.value * 60000 + setSeconds.value * 1000).toLong()
+            setMinText(timerLength)
+            setSecText(timerLength)
+            window.dismiss()
+        }
+        window.showAtLocation(openSetTime, Gravity.CENTER,0,0)
     }
+
     companion object{
         const val TAG = "TeaTimerFragment"
     }
